d029909539a50e9962483f61be55456d65524511
Md. Khairullah Gaurab <khairullah.gaurab@gmail.com>
2019-09-27 16:27:02 +0600

    Initial commit

---
+++ test world.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    vector<int> res = {1, 1, 1, 1, 1, 1};
+    for (auto a : res)
+        cout << a << " \n";
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string input;
+    cin >> input;
+    cout << input + "Hello! World" << endl;
+    return 0;
+}
